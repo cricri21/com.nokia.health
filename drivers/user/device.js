ad4966420dc8d424a8c12bf36d4c012df36e1bb0
Emile Nijssen <emilenijssen@gmail.com>
2017-12-06 00:28:49 +0100

    update

--- drivers/user/device.js
+++ drivers/user/device.js
@@ -4,48 +4,70 @@ const Homey = require('homey');
 const NokiaHealthApi = require('../../lib/NokiaHealthApi');
 
 class NokiaHealthDevice extends Homey.Device {
-	
+
 	onInit() {
-		
+
 		let data = this.getData();
 		this._userId = data.userId;
-		
+
 		let store = this.getStore();
 		this._api = new NokiaHealthApi();
+		this._api.on('measurements', this._onMeasurements.bind(this));
+		this._api.on('token', this._onToken.bind(this));
 		this._api.setToken( store.token );
-		this._api.createSubscription( this._userId ).catch( err => {
-			this.error(err);
-			this.setUnavailable( err );
-		})
-		this._api.on('measurement', this._onMeasurement.bind(this));
-		
-		this.sync();
+		this._api.createSubscription( this._userId )
+			.then(() => {
+				this.log('Subscribed to webhook events');
+				return this.sync();
+			})
+			.then(() => {
+				this.log('Successfully got latest measurement');
+			})
+			.catch( err => {
+				this.error(err);
+				this.setUnavailable( err );
+			})
 	}
-	
+
 	sync() {
 		this._api.getMeasurement({
 			userid: this._userId,
-			meastype: 'weight',
-			limit: 1
 		}).then( result => {
-			let measurement = NokiaHealthApi.parseMeasurementResult( result );
-			return this.setCapabilityValue('nh_measure_weight', measurement.value)
+			let measurements = NokiaHealthApi.parseMeasurementResult( result );
+			return this._syncMeasurements(measurements);
 		}).catch( this.error );
 	}
-	
+
+	_syncMeasurements( measurements ) {
+		let promises = [];
+		for( let type in measurements ) {
+			let value = measurements[type];
+			if( typeof value !== 'undefined' ) {
+				let capabilityId = `nh_measure_${type}`
+				if( this.hasCapability(capabilityId) ) {
+					let promise = this.setCapabilityValue(capabilityId, value)
+					promises.push( promise );
+				}
+			}
+		}
+		return Promise.all( promises );
+	}
+
 	onDeleted() {
-		this._api.deleteSubscription().catch( err => {
-			 this.error(err);
-		})
+		this._api.deleteSubscription().catch( this.error )
+		this._api.removeAllListeners();
 	}
-	
-	_onMeasurement( measurement ) {
-		this.log('_onMeasurement', measurement);
-				
-		if( measurement.type === 'weight' )
-			this.setCapabilityValue('nh_measure_weight', measurement.value).catch( this.error );
+
+	_onMeasurements( measurements ) {
+		this.log('_onMeasurements', measurements);
+		this._syncMeasurements( measurements ).catch( this.error );
 	}
-	
+
+	_onToken( token ) {
+		this.log('Refreshed OAuth2 token');
+		this.setStoreValue('token', token).catch( this.error );
+	}
+
 }
 
 module.exports = NokiaHealthDevice;
